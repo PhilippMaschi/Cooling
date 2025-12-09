587e0ed41861b7b7e146ffa4af42198863d82823
PhilippMaschi <phil.masch@hotmail.com>
2025-12-09 16:00:00 +0100

    deleted old visualziation files

--- utils/parquet.py
+++ utils/parquet.py
@@ -1,6 +1,7 @@
 import os
 from typing import List
 import pandas as pd
+from pathlib import Path
 
 
 def if_parquet_exists(file_name: str, folder: str) -> bool:
@@ -12,10 +13,9 @@ def write_parquet(data_frame: pd.DataFrame, file_name: str, folder: str) -> None
                           engine="auto", compression='gzip', index=False)
 
 
-def read_parquet(file_name: str, folder: str, column_names: List[str] = None) -> pd.DataFrame:
-    path_to_file = os.path.join(folder, f"{file_name}.parquet.gzip")
+def read_parquet(path_2_file: Path, column_names: List[str] = None) -> pd.DataFrame:
     if column_names:
-        df = pd.read_parquet(path=path_to_file, engine="auto", columns=column_names)
+        df = pd.read_parquet(path=path_2_file, engine="auto", columns=column_names)
     else:
-        df = pd.read_parquet(path=path_to_file, engine="auto")
+        df = pd.read_parquet(path=path_2_file, engine="auto")
     return df
